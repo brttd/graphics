4da5eabb74e9e0f52805ac79871e1d2dbabbf409
Brett Doyle <brettdoyle@protonmail.com>
2020-12-04 20:20:20 +0000

    Added source files

---
+++ includes/canvas/common.js
@@ -0,0 +1,4 @@
+var ctx = canvas.getContext("2d");
+
+var width = window.innerWidth;
+var height = window.innerHeight;
---
+++ includes/canvas/onResize.js
@@ -0,0 +1,26 @@
+function onResize() {
+    @@if (this.pixelScale !== undefined && this.pixelScale !== false) {
+        width = canvas.width = ~~(window.innerWidth / pixel_scale + 1);
+        height = canvas.height = ~~(window.innerHeight / pixel_scale + 1);
+
+        canvas.style.width = width * pixel_scale + 'px';
+        canvas.style.height = height * pixel_scale + 'px';
+    }
+
+    @@if (this.pixelScale === undefined || this.pixelScale == false) {
+        width = canvas.width = window.innerWidth;
+        height = canvas.height = window.innerHeight;
+    }
+
+    @@if (this.afterResize) {
+        afterResize();
+    }
+}
+
+@@if (this.pixelScale !== undefined && this.pixelScale !== false) {
+    canvas.style.imageRendering = 'pixelated';
+    document.body.style.overflow = 'hidden';
+}
+
+onResize();
+window.addEventListener("resize", onResize);
