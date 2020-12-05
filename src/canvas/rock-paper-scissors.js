62099a6ff144382b63119ba9ed2b78b09baef1d3
Brett Doyle <brettdoyle@protonmail.com>
2020-12-05 10:18:02 +0000

    Added rock paper scissors cellular automata

---
+++ src/canvas/rock-paper-scissors.js
@@ -0,0 +1,119 @@
+@@include("canvas/common.js");
+
+var pixel_scale = 5;
+
+var grid_old = [];
+var grid_new = [];
+var grid_tmp = null;
+
+var colors = [
+    [239, 71, 111],
+    [255, 209, 102],
+    [6, 214, 160],
+    [17, 138, 178],
+    [242, 146, 78],
+];
+
+var states = Math.random() < 0.5 ? 3 : colors.length;
+var winState = ~~(states / 2);
+
+var threshold = 3;
+
+var scores = [];
+scores.length = states;
+
+var offsets = [-1, -1, -1, 0, -1, 1, 0, 1, 1, 1, 1, 0, 1, -1, 0, -1];
+
+var offsetCount = offsets.length;
+
+var image_data;
+
+var i = 0;
+var x = 0;
+var y = 0;
+var o;
+
+function afterResize() {
+    grid_new.length = width * height;
+    grid_old.length = width * height;
+
+    image_data = ctx.getImageData(0, 0, width, height);
+
+    for (i = 0; i < grid_new.length; i++) {
+        grid_old[i] = ~~(Math.random() * states);
+        grid_new[i] = ~~(Math.random() * states);
+
+        image_data.data[i * 4 + 3] = 255;
+    }
+}
+
+@@include("canvas/onResize.js", { afterResize: true, pixelScale: true });
+
+function render() {
+    //Swap the two grids
+    grid_tmp = grid_new;
+
+    grid_new = grid_old;
+    grid_old = grid_tmp;
+
+    for (x = 0; x < width; x++) {
+        for (y = 0; y < height; y++) {
+            for (o = 0; o < states; o++) {
+                scores[o] = 0;
+            }
+
+            for (o = 0; o < offsetCount; o += 2) {
+                scores[
+                    grid_old[
+                        ((x + offsets[o] + width) % width) +
+                            ((y + offsets[o + 1] + height) % height) * width
+                    ]
+                ]++;
+            }
+
+            i = x + y * width;
+
+            grid_new[i] = grid_old[i];
+
+            for (o = 0; o < states; o++) {
+                if (
+                    o !== grid_old[i] &&
+                    scores[o] >= threshold &&
+                    (((grid_old[i] - o) % states) + states) % states > winState
+                ) {
+                    grid_new[i] = o;
+                    break;
+                }
+            }
+
+            image_data.data[i * 4] = colors[grid_new[i]][0];
+            image_data.data[i * 4 + 1] = colors[grid_new[i]][1];
+            image_data.data[i * 4 + 2] = colors[grid_new[i]][2];
+        }
+    }
+    /*
+    for (i = 0; i < grid_old.length; i++) {
+        grid_new[i] =
+            rules[
+                "" +
+                    ((i - width + width * height) % (width * height)) +
+                    "" +
+                    (~~(i / width) * width + ((i + 1) % width)) +
+                    "" +
+                    ((12 + width) % (width * height)) +
+                    "" +
+                    (~~(i / width) * width +
+                        ((((i - 1) % width) + width) % width))
+            ];
+
+        image_data.data[i * 4] = colors[grid_new[i]];
+        image_data.data[i * 4 + 1] = colors[grid_new[i]];
+        image_data.data[i * 4 + 2] = colors[grid_new[i]];
+
+        image_data.data[i * 4 + 3] = 255;
+    }*/
+
+    ctx.putImageData(image_data, 0, 0);
+}
+
+setInterval(render, 50);
