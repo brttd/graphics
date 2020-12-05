262fd4a69889769c18e865eff6196a28ea4a4988
Brett Doyle <brettdoyle@protonmail.com>
2020-12-05 14:49:14 +0000

    Fixed bug with invalid scale

--- includes/canvas/pixel_scale.js
+++ includes/canvas/pixel_scale.js
@@ -7,4 +7,13 @@
 
 if (window.location.search.startsWith('?scale=')) {
     pixel_scale = parseInt(window.location.search.split('&').pop().slice(7));
+
+    if (!isFinite(pixel_scale) || pixel_scale <= 0) {
+        @@if (this.pixel_scale == undefined) {
+            pixel_scale = 4;
+        }
+        @@if (this.pixel_scale !== undefined) {
+            pixel_scale = @@pixel_scale;
+        }
+    }
 }
