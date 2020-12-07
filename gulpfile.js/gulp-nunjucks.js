5f0c36a2c95003ab299b6b2b05181ec02da53311
Brett Doyle <brettdoyle@protonmail.com>
2020-12-07 21:18:17 +0000

    Added renderString method

--- gulpfile.js/gulp-nunjucks.js
+++ gulpfile.js/gulp-nunjucks.js
@@ -57,3 +57,53 @@ module.exports = function (options = {}) {
         }
     });
 };
+
+module.exports.render = function (options = {}) {
+    const opts = {};
+
+    opts.path = options.path || "";
+    opts.template = options.template || "";
+
+    const env = new nunjucks.Environment(
+        new nunjucks.FileSystemLoader(opts.path)
+    );
+
+    return through.obj(function (file, _, cb) {
+        if (file.isStream()) {
+            this.emit(
+                "error",
+                new Error("gulp-nunjucks: " + "Streams are not supported!")
+            );
+            return cb();
+        }
+
+        if (file.isNull()) {
+            return cb(null, file);
+        }
+
+        file.path = gutil.replaceExtension(file.path, ".html");
+
+        if (file.isBuffer()) {
+            env.renderString(String(file.contents), file.data, (err, res) => {
+                if (err) {
+                    this.emit(
+                        "error",
+                        new Error("gulp-nunjucks: " + "Render error:" + err)
+                    );
+
+                    return cb();
+                }
+
+                file.contents = Buffer.from(res);
+
+                this.push(file);
+
+                cb();
+            });
+        } else {
+            this.push(file);
+
+            cb();
+        }
+    });
+};
