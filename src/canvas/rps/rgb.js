ef09e6f361b1a03d7a0fcda92bacd272165e4bcf
Brett Doyle <brettdoyle@protonmail.com>
2020-12-08 18:25:57 +0000

    Updated algorithim

--- src/canvas/rps/rgb.js
+++ src/canvas/rps/rgb.js
@@ -17,11 +17,13 @@ var i = 0;
 var n = 0;
 var x = 0;
 var y = 0;
-var z = 0
+var z = 0;
 
 var t = 0;
-
 var f = 0;
+
+var b = 16;
+var r = 64;
 
 var o;
 
@@ -33,17 +35,16 @@ function afterResize() {
         x = Math.random();
         y = Math.random();
         z = Math.random();
-        n = 0.95 + (x + y + z) * 0.05;
 
-        image_data_new.data[i * 4 + 0] = ~~(255 * x / n);
-        image_data_new.data[i * 4 + 1] = ~~(255 * y / n);
-        image_data_new.data[i * 4 + 2] = ~~(255 * z / n);
+        image_data_new.data[i * 4 + 0] = ~~(255 * x);
+        image_data_new.data[i * 4 + 1] = ~~(255 * y);
+        image_data_new.data[i * 4 + 2] = ~~(255 * z);
 
         image_data_new.data[i * 4 + 3] = 255;
 
-        image_data_old.data[i * 4 + 0] = ~~(255 * x / n);
-        image_data_old.data[i * 4 + 1] = ~~(255 * y / n);
-        image_data_old.data[i * 4 + 2] = ~~(255 * z / n);
+        image_data_old.data[i * 4 + 0] = ~~(255 * x);
+        image_data_old.data[i * 4 + 1] = ~~(255 * y);
+        image_data_old.data[i * 4 + 2] = ~~(255 * z);
 
         image_data_old.data[i * 4 + 3] = 255;
     }
@@ -51,23 +52,35 @@ function afterResize() {
 
 @@include("canvas/onResize.js", { afterResize: true, pixel_scale: true });
 
+var totals = [0, 0, 0];
+
+//DEBUG:
+//var debug = document.createElement("p");
+//debug.style =
+//    "position: absolute;top: 5px;left:5px;background:rgba(0,0,0,0.5);color:white;";
+//document.body.appendChild(debug);
+
 function render() {
+    //DEBUG:
+    //totals[0] = 0;
+    //totals[1] = 0;
+    //totals[2] = 0;
+
     for (x = 0; x < width; x++) {
         for (y = 0; y < height; y++) {
-            scores[0] = 255;
-            scores[1] = 255;
-            scores[2] = 255;
-            boost[0] = Math.random() * 8;
-            boost[1] = Math.random() * 8;
-            boost[2] = Math.random() * 8;
+            scores[0] = 0;
+            scores[1] = 0;
+            scores[2] = 0;
 
-            //console.log(x, y, 0);
+            boost[0] = Math.random() * b;
+            boost[1] = Math.random() * b;
+            boost[2] = Math.random() * b;
 
             i = (x + y * width) * 4;
 
-            //image_data_new.data[i + 0] = image_data_old.data[i + 0];
-            //image_data_new.data[i + 1] = image_data_old.data[i + 1];
-            //image_data_new.data[i + 2] = image_data_old.data[i + 2];
+            image_data_new.data[i + 0] = image_data_old.data[i + 0];
+            image_data_new.data[i + 1] = image_data_old.data[i + 1];
+            image_data_new.data[i + 2] = image_data_old.data[i + 2];
 
             t = 0;
             f = 0;
@@ -84,38 +97,68 @@ function render() {
             ) {
                 //green highest
                 t = 1;
-            } else {
+            } else if (
+                image_data_new.data[i + 2] > image_data_new.data[i + 0] &&
+                image_data_new.data[i + 2] > image_data_new.data[i + 1]
+            ) {
                 //blue highest
                 t = 2;
+            } else {
+                //All equal = choose random
+                t = ~~(Math.random() * 3);
             }
 
-            //for (o = 0; o < offsetCount; o += 2) {
-            o = ~~(Math.random() * (offsetCount / 2)) * 2;
+            //DEBUG:
+            //totals[t] += 1;
 
-            n =
-                (((x + offsets[o] + width) % width) +
-                    ((y + offsets[o + 1] + height) % height) * width) *
-                4;
+            for (o = 0; o < offsetCount; o += 2) {
+                n =
+                    (((x + offsets[o] + width) % width) +
+                        ((y + offsets[o + 1] + height) % height) * width) *
+                    4;
 
-            if (
-                image_data_old.data[n + ((t + 1) % 3)] + boost[(t + 1) % 3] >=
-                    image_data_old.data[i + ((t + 0) % 3)] + boost[t] &&
-                image_data_old.data[n + ((t + 0) % 3)] < scores[(t + 0) % 3]
-            ) {
-                scores[0] = image_data_old.data[n + 0];
-                scores[1] = image_data_old.data[n + 1];
-                scores[2] = image_data_old.data[n + 2];
+                if (
+                    image_data_old.data[n + ((t + 1) % 3)] +
+                        boost[(t + 1) % 3] >=
+                    image_data_old.data[i + ((t + 0) % 3)] + boost[t]
+                ) {
+                    scores[0] += image_data_old.data[n + 0];
+                    scores[1] += image_data_old.data[n + 1];
+                    scores[2] += image_data_old.data[n + 2];
 
-                f = 1;
-
-                //break;
+                    f += 1;
+                }
             }
-            //}
 
-            if (f) {
-                image_data_new.data[i + 0] = scores[0];
-                image_data_new.data[i + 1] = scores[1] * 0.999;
-                image_data_new.data[i + 2] = scores[2] * 0.999;
+            if (f >= 1 + Math.random() * 1.2) {
+                scores[0] /= f;
+                scores[1] /= f;
+                scores[2] /= f;
+
+                n = Math.min(
+                    1.021,
+                    Math.max(
+                        1,
+                        scores[0] / 255 + scores[1] / 255 + scores[2] / 255
+                    )
+                );
+
+                scores[0] /= n;
+                scores[1] /= n;
+                scores[2] /= n;
+
+                image_data_new.data[i + 0] = Math.max(
+                    0,
+                    Math.min(255, scores[0] + Math.random() * r - r * 0.5)
+                );
+                image_data_new.data[i + 1] = Math.max(
+                    0,
+                    Math.min(255, scores[1] + Math.random() * r - r * 0.5)
+                );
+                image_data_new.data[i + 2] = Math.max(
+                    0,
+                    Math.min(255, scores[2] + Math.random() * r - r * 0.5)
+                );
             }
         }
     }
@@ -127,8 +170,13 @@ function render() {
     }
 
     ctx.putImageData(image_data_new, 0, 0);
+
+    //DEBUG:
+    //debug.textContent = JSON.stringify(totals);
+
+    requestAnimationFrame(render);
 }
 
-//render();
+render();
 
-setInterval(render, 100);
+//setInterval(render, 10);
