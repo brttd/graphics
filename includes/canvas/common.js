e6eef63fdcd13dd28c9b84c05567811ca43b6599
Brett Doyle <brettdoyle@protonmail.com>
2021-01-02 16:12:20 +0000

    Added poyfills for Date.now and requestAnimationFrame

--- includes/canvas/common.js
+++ includes/canvas/common.js
@@ -1,3 +1,40 @@
+if (!Date.now) {
+    Date.now = function () {
+        return new Date().getTime();
+    };
+}
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// MIT license
+
+(function () {
+    var lastTime = 0;
+    var vendors = ["ms", "moz", "webkit", "o"];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame =
+            window[vendors[x] + "RequestAnimationFrame"];
+        window.cancelAnimationFrame =
+            window[vendors[x] + "CancelAnimationFrame"] ||
+            window[vendors[x] + "CancelRequestAnimationFrame"];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function (callback) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+})();
+
 var ctx = canvas.getContext("2d");
 
 var width = window.innerWidth;
