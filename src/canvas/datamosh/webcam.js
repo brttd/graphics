669d50352b90e1a4f4d0ab9c681378b3ffe68cc2
Brett Doyle <brett@netdreams.co.uk>
2021-07-19 16:50:02 +0100

    Added webcam version

---
+++ src/canvas/datamosh/webcam.js
@@ -0,0 +1,224 @@
+var video = document.createElement("video");
+video.crossOrigin = 'Anonymous';
+video.style.display = 'none';
+video.loop = true;
+
+var painterCanvas = document.createElement('canvas');
+var painterCtx = painterCanvas.getContext('2d');
+
+var videoScale = 1.1;
+
+@@include("canvas/common.js");
+@@include("canvas/pixel_scale.js", { pixel_scale: 2 });
+@@include("canvas/onResize.js", { afterResize: true, pixel_scale: true });
+
+document.body.appendChild(video);
+
+video.volume = 0;
+
+var imageData = null;
+var imageDataPaint = null;
+var prevImageDataPaint = null;
+var prevImageData = null;
+
+var aspect = 1;
+
+var videoWidth = 0;
+var videoHeight = 0;
+
+var scale = 1;
+
+var i = 0;
+var j = 0;
+var idx = 0;
+
+var t = 0;
+var t1 = 0;
+var t2 = 0;
+
+var sourceX = 0;
+var sourceY = 0;
+
+var sourceW = 0;
+var sourceH = 0;
+
+var coordMultiplier = 1;
+
+var pixelValues = false;
+var totalPixels = 1;
+
+var isRendering = false;
+
+function afterResize() {
+    aspect = width / height;
+
+    totalPixels = width * height;
+
+    painterCanvas.width = width;
+    painterCanvas.height = height;
+
+    videoWidth = video.videoWidth;
+    videoHeight = video.videoHeight;
+
+    if (!prevImageData) {
+        imageData = ctx.createImageData(width, height);
+        prevImageData = ctx.createImageData(width, height);
+        prevImageDataPaint = ctx.createImageData(width, height);
+    }
+
+    if (!pixelValues || pixelValues.length !== totalPixels) {
+        pixelValues = new Uint8Array(totalPixels);
+    }
+
+    if (videoWidth === 0 || videoHeight === 0) return;
+
+    videoAspect = videoWidth / videoHeight;
+
+    scale = 1;
+
+    if (aspect <= videoAspect) {
+        //screen is more landscape than video
+        scale = height / videoHeight;
+    } else {
+        //Screen is more portrait than video
+        scale = width / videoWidth;
+    }
+
+    scale *= videoScale;
+
+    sourceW = width / scale;
+    sourceH = height / scale;
+
+    sourceX = Math.round(videoWidth * 0.5 - sourceW * 0.5);
+    sourceY = Math.round(videoHeight * 0.5 - sourceH * 0.5);
+
+    coordMultiplier = Math.round(width / 32);
+
+    //coordMultiplier = 5;
+}
+
+function render() {
+    //painterCtx.fillStyle = 'black';
+    //painterCtx.fillRect(0, 0, width, height);
+
+    painterCtx.drawImage(
+        video,
+        sourceX, sourceY,
+        sourceW, sourceH,
+        0, 0, width, height
+    );
+    imageData = painterCtx.getImageData(0, 0, width, height);
+    imageDataPaint = ctx.createImageData(width, height);
+
+    t1 = 0;
+
+    for (i = 0; i < imageDataPaint.data.length; i += 4) {
+        j = Math.max(imageData.data[i + 0], imageData.data[i + 1], imageData.data[i + 2]) * 0.85 +
+            Math.min(prevImageDataPaint.data[i + 0], prevImageDataPaint.data[i + 1], prevImageDataPaint.data[i + 2]) * 0.15;
+        
+        j = pixelValues[idx] = pixelValues[((idx - 1) + totalPixels) % totalPixels] * 0.3 +
+                                pixelValues[((idx - width) + totalPixels) % totalPixels] * 0.3 + j * 0.4;
+        idx += 1;
+        
+        t1 += ~~(Math.min(prevImageData.data[i + 0], prevImageData.data[i + 1], prevImageData.data[i + 2]) / 192);
+
+        imageDataPaint.data[i + 3] = 255;
+
+        if (j < 51) {
+            //Scanline Datamosh
+            imageDataPaint.data[i + 0] = imageData.data[(i + 0 + t1 * 4) % imageData.data.length];
+            imageDataPaint.data[i + 1] = imageData.data[(i + 1 + t1 * 4) % imageData.data.length];
+            imageDataPaint.data[i + 2] = imageData.data[(i + 2 + t1 * 4) % imageData.data.length];
+        } else if (j < 104) {
+            //Color accumulate & wrap
+            imageDataPaint.data[i + 0] = ((imageData.data[i + 0] + prevImageData.data[i + 0]) + 256) % 256;
+            imageDataPaint.data[i + 1] = ((imageData.data[i + 1] + prevImageData.data[i + 1]) + 256) % 256;
+            imageDataPaint.data[i + 2] = ((imageData.data[i + 2] + prevImageData.data[i + 2]) + 256) % 256;
+            
+            imageDataPaint.data[i + 0] = (imageDataPaint.data[i + 0] * .4 + prevImageDataPaint.data[i + 0] * 0.7) % 256;
+            imageDataPaint.data[i + 1] = (imageDataPaint.data[i + 1] * .4 + prevImageDataPaint.data[i + 1] * 0.7) % 256;
+            imageDataPaint.data[i + 2] = (imageDataPaint.data[i + 2] * .4 + prevImageDataPaint.data[i + 2] * 0.7) % 256;
+            
+        } else if (j < 153) {
+            //Pixel Shift - horizontal duplicate / glass emboss
+            t = Math.round(Math.min(imageData.data[i + 0], imageData.data[i + 1], imageData.data[i + 2]) / 10) * 16;
+            
+            imageDataPaint.data[i + 0] = imageData.data[(i + 0 + (t - 128) * 20) % imageDataPaint.data.length];
+            imageDataPaint.data[i + 1] = imageData.data[(i + 1 + (t - 128) * 20) % imageDataPaint.data.length];
+            imageDataPaint.data[i + 2] = imageData.data[(i + 2 + (t - 128) * 20) % imageDataPaint.data.length];
+        } else if (j < 204) {
+            /*
+            //Pixel Shift - horizontal duplicate / glass emboss
+            //(Version 2)
+            t = Math.round(Math.max(imageData.data[i + 0], imageData.data[i + 1], imageData.data[i + 2]) / 10) * 16;
+            
+            imageDataPaint.data[i + 0] = imageData.data[(i + 0 + (t - 128) * coordMultiplier) % imageDataPaint.data.length];
+            imageDataPaint.data[i + 1] = imageData.data[(i + 1 + (t - 128) * coordMultiplier) % imageDataPaint.data.length];
+            imageDataPaint.data[i + 2] = imageData.data[(i + 2 + (t - 128) * coordMultiplier) % imageDataPaint.data.length];
+            */
+            //Scanline Datamosh
+            imageDataPaint.data[i + 0] = imageData.data[(i + 0 + t1 * 4) % imageData.data.length];
+            imageDataPaint.data[i + 1] = imageData.data[(i + 1 + t1 * 4) % imageData.data.length];
+            imageDataPaint.data[i + 2] = imageData.data[(i + 2 + t1 * 4) % imageData.data.length];
+        } else {
+            //Contrast Chaos + Noise
+            imageDataPaint.data[i + 0] = ((imageData.data[i + 0] % prevImageData.data[i + 0]) + 256) % 256;
+            imageDataPaint.data[i + 1] = ((imageData.data[i + 1] % prevImageData.data[i + 1]) + 256) % 256;
+            imageDataPaint.data[i + 2] = ((imageData.data[i + 2] % prevImageData.data[i + 2]) + 256) % 256;
+            
+            imageDataPaint.data[i + 0] = (imageDataPaint.data[i + 0] * .5 + prevImageDataPaint.data[i + 0] * 0.6) % 256;
+            imageDataPaint.data[i + 1] = (imageDataPaint.data[i + 1] * .5 + prevImageDataPaint.data[i + 1] * 0.6) % 256;
+            imageDataPaint.data[i + 2] = (imageDataPaint.data[i + 2] * .5 + prevImageDataPaint.data[i + 2] * 0.6) % 256;
+        }
+
+        //imageDataPaint.data[i] = imageDataPaint.data[i + 1] = imageDataPaint.data[i + 2] = Math.round(j / 51) * 51;
+    }
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.putImageData(imageDataPaint, 0, 0);
+
+    prevImageData = imageData;
+    prevImageDataPaint = imageDataPaint;
+
+    video.requestVideoFrameCallback(render);
+}
+
+video.addEventListener('canplay', function() {
+    console.log('playing');
+    video.play();
+
+    afterResize();
+
+    if (!isRendering) {
+        isRendering = true;
+
+        video.requestVideoFrameCallback(render);
+    }
+});
+
+function start() {
+    navigator.mediaDevices.getUserMedia({
+
+        audio: false,
+        video: true,
+    }).then(function(stream) {
+        video.srcObject = stream;
+
+        video.play();
+        
+
+        afterResize();
+
+        if (!isRendering) {
+            isRendering = true;
+
+            video.requestVideoFrameCallback(render);
+        }
+    }).catch(function(err) {
+        console.log('ERROR', err);
+    })
+}
+
+start();
